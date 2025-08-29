4dffcea3480c23f64a2d42bf3bc450e434d384ed
Gabriel Reynoso <garey91@gmail.com>
2025-08-29 23:15:58 +0200

    Update ProductDetailPage to fetch product data from Supabase and display details

--- src/app/product-detail-page/[id]/page.js
+++ src/app/product-detail-page/[id]/page.js
@@ -1,37 +1,57 @@
 import { getStoryblokApi } from "@/lib/storyblok";
 import getSbVersion from "@/utils/getSbVersion";
 import Image from "next/image";
+import { supabase } from "@/lib/supabaseClient";
 
 export default async function ProductDetailPage({ params }) {
-  const { id } = params;
+  const { id } = await params;
+  console.log(id);
+
+  const { data: product, error } = await supabase
+    .from("product")
+    .select()
+    .eq("id", id)
+    .single();
+
+  if (error || !product) {
+    return <h1>Product not found</h1>;
+  }
+
+  console.log(product);
 
   return (
-    <section className="h-[100vh] flex w-full">
-      <div className="flex-1">
-        <Image src={{}} alt={{}} width={{}} height={{}} />
+    <section className="h-[100vh] flex flex-col md:flex-row w-full">
+      <div className="flex-1 flex items-center justify-center">
+        <Image src={product.image_url} alt={{}} width={400} height={400} />
       </div>
-      <div className="flex-1 flex items-center">
+      <div className="flex-1 flex items-center justify-center">
         <div>
-          <h2 className="text-3xl mb-2">Mens Winter Jacket</h2>
-          <p className="mb-2">€99</p>
-          <p className="mb-2">
-            Revamp your style with the latest designer trends in men’s clothing
-            or achieve a perfectly curated wardrobe thanks to our line-up of
-            timeless pieces.{" "}
-          </p>
+          <h2 className="text-3xl mb-2">{product.name}</h2>
+          <p className="mb-2">€{product.price}</p>
+          <p className="mb-2">{product.description}</p>
           <div className="mb-2">
             <p>Color</p>
             <div className="flex">
-              <div className="border-1 rounded-full w-10 h-10"></div>
-              <div className="border-1 rounded-full w-10 h-10"></div>
+              {product.color.map((color, i) => (
+                <div
+                  key={i}
+                  className="border-1 rounded-full w-10 h-10"
+                  style={{ backgroundColor: color }}
+                ></div>
+              ))}
             </div>
           </div>
           <div className="mb-2">
             <p>Size</p>
             <div className="flex">
-              <div className="border-1 rounded-md w-10 h-10"></div>
-              <div className="border-1 rounded-md w-10 h-10"></div>
-              <div className="border-1 rounded-md w-10 h-10"></div>
+              {product.size.map((size, i) => (
+                <div
+                  key={i}
+                  className="border-1 rounded-md w-10 h-10 flex justify-center items-center"
+                >
+                  <p>{size.toUpperCase()}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div>
