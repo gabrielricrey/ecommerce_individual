2c12def5643b97a85ebf3be79c96e78fc2a9ed76
Gabriel Reynoso <garey91@gmail.com>
2025-09-03 15:39:12 +0200

    Enhance Header component with dropdown functionality and improved layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,72 @@
+"use client";
+
 import Link from "next/link";
 import {
   MagnifyingGlassIcon,
   ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
 import { cn } from "@/utils/cn";
+import { useState } from "react";
 
 export default function Header({ data, darkMode }) {
+  const [hovered, setHovered] = useState(false);
+  const [subHovered, setSubHovered] = useState(false);
+
   const headerClasses = cn(
-    "w-full border-black border-b-1 border-opacity-50 flex justify-center items-center bg-[#EFF2F6] h-[60px]",
+    "w-full border-black border-b-1 border-opacity-50 flex justify-center items-start bg-[#EFF2F6]",
     { "bg-black text-white": darkMode }
   );
+
   const seachInputClasses = "outline-none";
+
   return (
     <header className={headerClasses}>
-      <nav className="flex w-[1400px] gap-10 px-5 items-center justify-between">
-        <div className="flex-1 flex gap-10 items-center">
+      <nav className="flex w-[1400px] gap-10 px-5 justify-between py-3 transition-all duration-300">
+        <div className="flex-1 flex gap-10 items-start">
+          {/* LOGO */}
           <Link href="/" className="font-bold text-lg">
             {data.logo[0].label}
           </Link>
-          <Link href={{}}>{data.header_links[0].name}</Link>
-          <Link href={{}}>{data.header_links[1].name}</Link>
+
+          {/* PRODUCTS DROPDOWN */}
+          <div
+            className="relative"
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <Link href="#" className="block">
+              {data.header_links[0].name}
+            </Link>
+
+            {hovered && (
+              <div className="mt-2 flex flex-col p-2">
+                <div
+                  className="relative"
+                  onMouseEnter={() => setSubHovered(true)}
+                  onMouseLeave={() => setSubHovered(false)}
+                >
+                  <Link href="#">Sweaters</Link>
+                  {subHovered && (
+                    <div className="mt-2 flex flex-col p-2">
+                      <Link href="#">Man</Link>
+                      <Link
+                        href="#"
+                        onClick={(e) => e.preventDefault()}
+                        className="line-through text-gray-400 cursor-not-allowed pointer-events-none"
+                      >
+                        Woman
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* ANOTHER LINK */}
+          <Link href="#">{data.header_links[1].name}</Link>
+
+          {/* SEARCH */}
           <div className="flex-1 flex gap-3 justify-start">
             <MagnifyingGlassIcon className="size-6" />
             <input
@@ -29,6 +76,8 @@ export default function Header({ data, darkMode }) {
             />
           </div>
         </div>
+
+        {/* BAG */}
         <ShoppingBagIcon className="size-6 cursor-pointer" />
       </nav>
     </header>
